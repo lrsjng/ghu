0114365d80baa0802f23aad040c6a0eb75037f0a
Lars Jung <lrsjng@gmail.com>
2015-12-17 02:42:19 +0100

    Back to node module syntax.

--- test/lib/util/misc.js
+++ test/lib/util/misc.js
@@ -2,35 +2,35 @@ const {test, assert, insp} = require('scar');
 const util = require('../../../lib/util');
 
 /* eslint-disable no-multi-spaces, no-new-wrappers, no-array-constructor, func-names */
-// value, isNumber, isString, isArray, isBuffer, isFn
+// value, isNumber, isString, isFn
 const X = true;
 const _ = false;
 const FIXTURES = [
-    [undefined,        _, _, _, _, _],
-    [null,             _, _, _, _, _],
-    [true,             _, _, _, _, _],
-    [false,            _, _, _, _, _],
-    [NaN,              X, _, _, _, _],
-    [0,                X, _, _, _, _],
-    [1,                X, _, _, _, _],
-    ['undefined',      _, X, _, _, _],
-    ['null',           _, X, _, _, _],
-    ['true',           _, X, _, _, _],
-    ['false',          _, X, _, _, _],
-    ['NaN',            _, X, _, _, _],
-    ['',               _, X, _, _, _],
-    ['0',              _, X, _, _, _],
-    ['1',              _, X, _, _, _],
-    [new String(),     _, X, _, _, _],
-    [[],               _, _, X, _, _],
-    [new Array(),      _, _, X, _, _],
-    [new Buffer(0),    _, _, _, X, _],
-    [() => null,       _, _, _, _, X],
-    [function () {},   _, _, _, _, X],
-    [test,             _, _, _, _, X],
-    [{},               _, _, _, _, _],
-    [/a/,              _, _, _, _, _],
-    [new Error(),      _, _, _, _, _]
+    [undefined,        _, _, _],
+    [null,             _, _, _],
+    [true,             _, _, _],
+    [false,            _, _, _],
+    [NaN,              X, _, _],
+    [0,                X, _, _],
+    [1,                X, _, _],
+    ['undefined',      _, X, _],
+    ['null',           _, X, _],
+    ['true',           _, X, _],
+    ['false',          _, X, _],
+    ['NaN',            _, X, _],
+    ['',               _, X, _],
+    ['0',              _, X, _],
+    ['1',              _, X, _],
+    [new String(),     _, X, _],
+    [[],               _, _, _],
+    [new Array(),      _, _, _],
+    [new Buffer(0),    _, _, _],
+    [() => null,       _, _, X],
+    [function () {},   _, _, X],
+    [test,             _, _, X],
+    [{},               _, _, _],
+    [/a/,              _, _, _],
+    [new Error(),      _, _, _]
 ];
 /* eslint-enable */
 
@@ -60,24 +60,6 @@ FIXTURES.forEach(x => {
     const val = x[0];
     const exp = x[3];
 
-    test(`util.isArray(${insp(val)}) === ${insp(exp)}`, () => {
-        assert.equal(util.isArray(val), exp);
-    });
-});
-
-FIXTURES.forEach(x => {
-    const val = x[0];
-    const exp = x[4];
-
-    test(`util.isBuffer(${insp(val)}) === ${insp(exp)}`, () => {
-        assert.equal(util.isBuffer(val), exp);
-    });
-});
-
-FIXTURES.forEach(x => {
-    const val = x[0];
-    const exp = x[5];
-
     test(`util.isFn(${insp(val)}) === ${insp(exp)}`, () => {
         assert.equal(util.isFn(val), exp);
     });
@@ -85,7 +67,7 @@ FIXTURES.forEach(x => {
 
 FIXTURES.forEach(x => {
     const val = x[0];
-    const isFn = x[5];
+    const isFn = x[3];
 
     if (isFn) {
         test(`util.asFn(${insp(val)}) === ${insp(val)}`, () => {

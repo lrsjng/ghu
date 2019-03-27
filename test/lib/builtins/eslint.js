cc4b79e35182854b21d4456bef419f3ca9d847e5
Lars Jung <lrsjng@gmail.com>
2019-03-27 01:16:50 +0100

    Update eslint.

--- test/lib/builtins/eslint.js
+++ test/lib/builtins/eslint.js
@@ -1,3 +1,4 @@
+const {resolve} = require('path');
 const {test, assert} = require('scar');
 const {eslint} = require('../../../lib/ghu');
 
@@ -16,20 +17,21 @@ test('lib.eslint() - no objects', () => {
 });
 
 test('lib.eslint() - empty', () => {
+    const assets = resolve(__dirname, 'assets');
+    const a = resolve(assets, 'a.txt');
     const fn = eslint({showReport: false});
-    const content = '';
-    const expected = '';
-    const objs = [{source: 'a.js', content}];
+    const objs = [{source: a}];
 
     return fn(objs).then(val => {
-        assert.deepEqual(val, [{source: 'a.js', content: expected}]);
+        assert.deepEqual(val, objs);
     });
 });
 
 test('lib.eslint() - does not alter objects', () => {
+    const assets = resolve(__dirname, 'assets');
+    const b = resolve(assets, 'b.txt');
     const fn = eslint({showReport: false});
-    const content = 'const a = 1;';
-    const objs = [{source: 'a.js', content}];
+    const objs = [{source: b}];
 
     return fn(objs).then(val => {
         assert.deepEqual(val, objs);
